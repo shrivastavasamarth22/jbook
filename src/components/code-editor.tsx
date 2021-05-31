b73a4f202c14e7169d429f19b758215c14229fa8
Samarth Shrivastava <26189104+shrivastavasamarth22@users.noreply.github.com>
2021-06-01 00:57:50 +0530

    Added some styling to the editor and the format button

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -3,6 +3,8 @@ import MonacoEditor, {EditorDidMount} from '@monaco-editor/react';
 import prettier from 'prettier'
 import parser from 'prettier/parser-babel'
 
+import './code-editor.css'
+
 interface CodeEditorProps {
     initialValue: string;
 
@@ -32,14 +34,14 @@ const CodeEditor: FC<CodeEditorProps> = ({initialValue, onChange}) => {
             useTabs: false,
             semi: true,
             singleQuote: true
-        })
+        }).replace(/\n$/, '');
 
         // set the formatted value back in the editor
         editorRef.current.setValue(formatted)
     }
 
     return (
-        <div>
+        <div className={"editor-wrapper"}>
             <button className={"button button-format is-primary is-small"} onClick={onFormatClick}>Format</button>
             <MonacoEditor
                 height={"500px"}
